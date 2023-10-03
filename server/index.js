1da49c48cedc8a62a8505efe810a80bbbb0ed4c2
nandi1514 <nandiakash1415@gmail.com>
2023-10-03 15:17:33 +0530

    fixing cors error

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.use(
   cors({
     credentials: true,
     origin: `${process.env.CLIENT_URL}`,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
 app.use(express.json());
@@ -31,9 +32,9 @@ app.use("/images", express.static(path.join(__dirname, "/images")));
 connectDB();
 
 // Routes
-app.use('/', (req, res) => {
-  return res.status(200).json('Hermes App Server Working.')
-})
+app.use("/", (req, res) => {
+  return res.status(200).json("Hermes App Server Working.");
+});
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
